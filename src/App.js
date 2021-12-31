cbea4367e95c7c59c6ebd8fcb82762408a5f5f96
Antonio <atcarter005@gmail.com>
2021-12-31 05:26:27 -0600

    Outline switch for routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route exact path="/" component={}/>
+      </Switch>
     </div>
   );
 }
