4f8492ca297a2bc9fc797571462cccb1949e6073
Antonio <atcarter005@gmail.com>
2022-01-02 20:41:24 -0600

    Outline for review component

---
+++ src/components/Review.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+class Review extends Component {
+
+  render() {
+
+    return (
+      <div id=''>
+        <h3>Username Rating</h3>
+        <p>Description</p>
+      </div>
+    )
+  }
+
+}
+
+export default Review;
