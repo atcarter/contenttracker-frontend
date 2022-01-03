ac90ef65e0304762e983a2cc7ce0d1152e4dd613
Antonio <atcarter005@gmail.com>
2022-01-03 03:34:31 -0600

    Add three navlinks on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom";
 export default NavBar = () => {
   return (
     <div>
-      
+      <NavLink to="/about">About</NavLink>
+      <NavLink exact to="/contents">Content List</NavLink>
+      <NavLink to="/contents/new">Add New Content</NavLink>
     </div>
   )
 }
