dfc59784438bfb5ceaa5c3f054ed0e186ca63cd6
Antonio <atcarter005@gmail.com>
2022-01-03 03:42:48 -0600

    import Navbar into App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Navbar />
         <Switch>
           <Route exact path="/contents" component={ContentList}/>
           <Route exact path="/about" component={About}/>
