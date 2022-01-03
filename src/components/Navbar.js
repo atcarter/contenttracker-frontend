21848a8a89c11534988dafd9e9123a281f51ef5e
Antonio <atcarter005@gmail.com>
2022-01-03 03:43:01 -0600

    convert navbar into a class to resolve error messages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
+import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 
-export default NavBar = () => {
-  return (
-    <div>
-      <NavLink to="/about">About</NavLink>
-      <NavLink exact to="/contents">Content List</NavLink>
-      <NavLink to="/contents/new">Add New Content</NavLink>
-    </div>
-  )
+class Navbar extends Component {
+
+  render(){
+    return (
+      <div>
+        <NavLink to="/about">About</NavLink>
+        <NavLink exact to="/contents">Content List</NavLink>
+        <NavLink to="/contents/new">Add New Content</NavLink>
+      </div>
+    )
+  }
+
 }
+
+export default Navbar;
