53610f60e4e339f3d5dfe38f3ce87b3de06d283c
Antonio <atcarter005@gmail.com>
2022-01-02 19:59:54 -0600

    outline for content component

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+
+  render() {
+
+    return (
+      <div id=''>
+        <h3>Content Title</h3>
+        <p>Description</p>
+      </div>
+    )
+  }
+
+}
+
+export default Content;
