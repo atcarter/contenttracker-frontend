075bf42c5f80a96a2d3c055e57993175663e6da1
Antonio <atcarter005@gmail.com>
2022-01-03 08:02:34 -0600

    add create content action

--- src/actions/contentActions.js
+++ src/actions/contentActions.js
@@ -8,3 +8,26 @@ export const fetchContents = () => {
       .catch(fail => alert(fail));
   };
 };
+
+export const createContent = (newContent) => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/contents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        title: newContent.title,
+        year: newContent.year,
+        content_type: newContent.content_type,
+        details: newContent.details
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: "CREATE_CONTENT", contents: data });
+      })
+      .catch(fail => alert(fail));
+  };
+};
