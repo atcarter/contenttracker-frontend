46bc7f33e7d56108e35efd26f2998192d83cfe1d
Antonio <atcarter005@gmail.com>
2022-01-03 02:42:21 -0600

    reverse id for content div

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ class Content extends Component {
   render() {
 
     return (
-      <div id={`${this.props.content_id}-content`}>
+      <div id={`content-${this.props.content_id}`}>
         <h3>{`${this.props.title} (${this.props.year})`}</h3>
         <p>{this.props.description}</p>
         <ReviewList reviews={this.props.reviews} content_id={this.props.content_id} />
