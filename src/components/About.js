11a8b78dd0238e3d0bf03095b74cdd3fdb170b85
Antonio <atcarter005@gmail.com>
2022-01-02 19:40:34 -0600

    skeleton info for about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 class About extends Component {
 
+  render() {
+    return (
+      <div className='about'>
+        <h2>About Page</h2>
+        <p>This is where all of the about page information goes.</p>
+      </div>
+    )
+  }
+
 }
 
 export default About;
