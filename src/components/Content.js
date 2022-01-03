05162dd7b10bfab9ea79186c052f2e71faa51252
Antonio <atcarter005@gmail.com>
2022-01-03 02:39:20 -0600

    Render function for Content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { ReviewList } from './ReviewList';
 
 class Content extends Component {
 
   render() {
 
     return (
-      <div id=''>
-        <h3>Content Title</h3>
-        <p>Description</p>
+      <div id={`${this.props.content_id}-content`}>
+        <h3>{`${this.props.title} (${this.props.year})`}</h3>
+        <p>{this.props.description}</p>
+        <ReviewList reviews={this.props.reviews} content_id={this.props.content_id} />
       </div>
     )
   }
