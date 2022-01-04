e23149f31dead5134c93b40a1d3b90c0be7f5796
Antonio <atcarter005@gmail.com>
2022-01-04 05:05:37 -0600

    finish content

--- src/components/ContentForm.js
+++ src/components/ContentForm.js
@@ -16,6 +16,19 @@ export default function ContentForm() {
     <div id='content-form-div'>
       <h2>New Content Form</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("title", { required: true })} />
+        {errors.title && "A title is required!"}
+
+        <input type="number" {...register("year", { required: true })} />
+        {errors.year && "A year is required!"}
+
+        <input {...register("content_type", { required: true })} />
+        {errors.content_type && "A content type is required!"}
+
+        <input {...register("details", { required: true })} />
+        {errors.details && "Details are required!"}
+
+        <input type="submit"/>
 
       </form>
     </div>
