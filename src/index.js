c5d5684c7864f9dcd360bd5c2efe10cf5d417406
Antonio <atcarter005@gmail.com>
2022-01-04 02:14:47 -0600

    change contentReducer to reducer and import the correct function into index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import contentReducer from './reducers/contentReducer';
+import reducer from './reducers/Reducer';
 
-const store = createStore(contentReducer, applyMiddleware(thunk)); //need to create/import reducer here
+const store = createStore(reducer, applyMiddleware(thunk)); //need to create/import reducer here
 
 
 ReactDOM.render(
