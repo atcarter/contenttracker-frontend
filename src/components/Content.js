8937e6c9d4c703e5315d70bab881ea83a13edeba
Antonio <atcarter005@gmail.com>
2022-01-04 03:22:25 -0600

    add an id prop to content

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,11 +6,11 @@ class Content extends Component {
   render() {
 
     return (
-      <div id={`content-${this.props.content_id}`}>
+      <div id={`content-${this.props.id}`}>
         <h3>{`${this.props.title} (${this.props.year})`}</h3>
         <i>{this.props.content_type}</i>
         <p>{this.props.details}</p>
-        <ReviewList reviews={this.props.reviews} content_id={this.props.content_id} />
+        <ReviewList reviews={this.props.reviews} content_id={this.props.id} />
       </div>
     )
   }
