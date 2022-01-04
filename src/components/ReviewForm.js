925f3376a897182522c52d11ad51a62a0e2f6465
Antonio <atcarter005@gmail.com>
2022-01-04 07:06:36 -0600

    draft prior to testing review form. added logic to prevent the form from taking in bad params and adding a review to content that doesn't exist.

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,42 +1,52 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { createReviews } from '../actions/reviewActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { createReview } from '../actions/reviewActions';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function ReviewForm() {
 
   const { register, handleSubmit, formState: { errors } } = useForm();
   const dispatch = useDispatch();
 
+  const contentID = useParams();
+  const parentContent = useSelector((state) => state.contents.filter((elem)=> elem.id === contentID))
+
   const onSubmit = data => {
     dispatch(createReview(data))
   }
 
-  return(
-    <div id='content-form-div'>
-      <h2>New Content Form</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor='title'>Title: </label>
-        <input id='title' name='title' {...register("title", { required: true })} />
-        {errors.title && "A title is required!"}<br/>
-
-        <label htmlFor='year'>Year: </label>
-        <input id='year' name='year' type="number" {...register("year", { required: true })} />
-        {errors.year && "A year is required!"}<br/>
-
-        <label htmlFor='content_type'>Content Type: </label>
-        <input id='content_type' name='content_type' {...register("content_type", { required: true })} />
-        {errors.content_type && "A content type is required!"}<br/>
-
-        <label htmlFor='details'>Details: </label>
-        <input id='details' name='details' {...register("details", { required: true })} />
-        {errors.details && "Details are required!"}<br/>
-
-        <input type="submit"/>
-
-      </form>
-    </div>
-  )
-
-
+  if (parentContent.length < 1) {
+    return(
+      <div>
+        <h1>THIS CONTENT DOESN'T EXIST</h1>
+      </div>
+    )
+  } else {
+    return(
+      <div id='review-form-div'>
+        <h2>New Review Form for {parentContent[0].title}</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor='username'>Username: </label>
+          <input id='username' name='username' {...register("username", { required: true })} />
+          {errors.username && "A username is required!"}<br/>
+  
+          <label htmlFor='rating'>Rating: </label>
+          <input id='rating' name='rating' type="number" {...register("rating", { min: 1, max: 5 })} />
+          {errors.rating && "A rating between 1-5 is required!"}<br/>
+  
+          <label htmlFor='description'>Description: </label>
+          <input id='description' name='description' {...register("description", { required: true })} />
+          {errors.description && "A description is required!"}<br/>
+  
+          {/* <label htmlFor='content_id'>Content Name: </label> */}
+          <input type="hidden" id='content_id' name='content_id'  value={parentContent[0].id} {...register("content_id", { required: true })} />
+          {/* {errors.content_id && "This cannot be changed"}<br/> */}
+  
+          <input type="submit"/>
+  
+        </form>
+      </div>
+    )
+  }
 }
