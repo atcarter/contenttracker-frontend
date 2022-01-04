a7fd48077f09d036d3074c355580d19e90a2b160
Antonio <atcarter005@gmail.com>
2022-01-04 05:34:05 -0600

    finish layout of new content

--- src/components/ContentForm.js
+++ src/components/ContentForm.js
@@ -5,7 +5,7 @@ import { createContent } from '../actions/contentActions';
 
 export default function ContentForm() {
 
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const dispatch = useDispatch();
 
   const onSubmit = data => {
@@ -16,17 +16,21 @@ export default function ContentForm() {
     <div id='content-form-div'>
       <h2>New Content Form</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("title", { required: true })} />
-        {errors.title && "A title is required!"}
+        <label htmlFor='title'>Title: </label>
+        <input id='title' name='title' {...register("title", { required: true })} />
+        {errors.title && "A title is required!"}<br/>
 
-        <input type="number" {...register("year", { required: true })} />
-        {errors.year && "A year is required!"}
+        <label htmlFor='year'>Year: </label>
+        <input id='year' name='year' type="number" {...register("year", { required: true })} />
+        {errors.year && "A year is required!"}<br/>
 
-        <input {...register("content_type", { required: true })} />
-        {errors.content_type && "A content type is required!"}
+        <label htmlFor='content_type'>Content Type: </label>
+        <input id='content_type' name='content_type' {...register("content_type", { required: true })} />
+        {errors.content_type && "A content type is required!"}<br/>
 
-        <input {...register("details", { required: true })} />
-        {errors.details && "Details are required!"}
+        <label htmlFor='details'>Details: </label>
+        <input id='details' name='details' {...register("details", { required: true })} />
+        {errors.details && "Details are required!"}<br/>
 
         <input type="submit"/>
 
