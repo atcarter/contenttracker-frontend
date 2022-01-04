3e258158d934e3c5bdb0b07936513c55951154e8
Antonio <atcarter005@gmail.com>
2022-01-04 04:45:21 -0600

    import useDispatch, createContent, and create consts for our functions

--- src/components/ContentForm.js
+++ src/components/ContentForm.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { createContent } from '../actions/contentActions';
 
 export default function ContentForm() {
 
+  const { register, handleSubmit, errors } = useForm();
+  const dispatch = useDispatch();
 
-  
+  const onSubmit = data => {
+    dispatch(createContent(data))
+  }
+
+
 }
