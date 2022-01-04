128dadfa4ff2bb7a836309261961fd0ae073dbc0
Antonio <atcarter005@gmail.com>
2022-01-04 01:51:41 -0600

    beginning of refactor using combineReducers

--- src/reducers/contentReducer.js
+++ src/reducers/contentReducer.js
@@ -1,11 +1,26 @@
+import { combineReducers } from "redux";
+
 export default function contentReducer(state = { contents: [], reviews: []}, action){
   switch (action.type) {
     case "GET_CONTENT":
       return{
         ...state,
-        contents: action.contents
+        books: [...state.books],
+        contents: [...state.contents, action.contents]
       };
       case "GET_REVIEWS":
+        return{
+          ...state,
+          contents: [...state.contents],
+          reviews: [...state.reviews, action.reviews]
+        };
+      case "CREATE_CONTENT":
+        return{
+          ...state,
+          reviews: [...state.reviews],
+          contents: [...state.contents, action.content]
+        };
+      case "DELETE_CONTENT":
         return{
           ...state,
           reviews: action.reviews
