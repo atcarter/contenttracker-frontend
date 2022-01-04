ea3a553b3ce37131fff78b9d01cbc9fc811f62a7
Antonio <atcarter005@gmail.com>
2022-01-04 05:54:48 -0600

    add styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 
+const link = {
+  padding: "20px",
+  background: "black",
+  color: "bisque",
+};
+
+const activeLink = {
+  padding: "20px",
+  background: "white",
+  color: "black",
+}
+
 class Navbar extends Component {
 
   render(){
     return (
       <div>
-        <NavLink to="/about">About</NavLink>
-        <NavLink exact to="/contents">Content List</NavLink>
-        <NavLink to="/contents/new">Add New Content</NavLink>
+        <NavLink to="/about" style={link} activeStyle={activeLink}>About</NavLink>
+        <NavLink exact to="/contents" style={link} activeStyle={activeLink}>Content List</NavLink>
+        <NavLink to="/contents/new" style={link} activeStyle={activeLink}>Add New Content</NavLink>
       </div>
     )
   }
