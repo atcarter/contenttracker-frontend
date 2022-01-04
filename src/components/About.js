70b5720d98ce4e95b4190eb6a458e344102be9ed
Antonio <atcarter005@gmail.com>
2022-01-04 10:55:48 -0600

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,8 @@ class About extends Component {
     return (
       <div className='about'>
         <h2>About Page</h2>
-        <p>This is where all of the about page information goes.</p>
+        <p>This is a content tracking web application that allows a user/users to create a list of content they plan to consume i.e. books, movies, videogames, etc. A user is able to view their content and their relevant reviews, add/delete content, and add/delete reviews.</p>
+        <a href='https://github.com/atcarter/contenttracker-frontend'>Here is the link to the frontend repository.</a>
       </div>
     )
   }
