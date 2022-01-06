06856d700a826f390d40989be284f79c20d67ad1
Antonio <atcarter005@gmail.com>
2022-01-06 11:28:03 -0600

    remove unused code/comments

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -10,8 +10,6 @@ export default reducer;
 function contentsReducer(state = [], action) {
   switch (action.type) {
     case "GET_CONTENT":
-      // console.log({...state, contents: action.contents})
-      // return {...state, contents: action.contents};
       return [...state, ...action.contents]
 
       case "CREATE_CONTENT":
@@ -28,7 +26,6 @@ function contentsReducer(state = [], action) {
 function reviewsReducer(state = [], action) {
   switch (action.type) {
     case "GET_REVIEWS":
-      // return{...state, reviews: action.reviews};
       return [...state, ...action.reviews]
 
 
@@ -43,66 +40,3 @@ function reviewsReducer(state = [], action) {
   }
 }
 
-
-// function contentsReducer(state = [], action) {
-//   switch (action.type) {
-//     case "GET_CONTENT":
-//       return[...state, action.contents];
-
-//       case "CREATE_CONTENT":
-//         return[...state, action.content];
-
-//       case "DELETE_CONTENT":
-//         return[...state].filter(elem => elem.id !== action.payload);
-  
-//     default:
-//       return state;
-//   }
-// }
-
-// function reviewsReducer(state = [], action) {
-//   switch (action.type) {
-//     case "GET_REVIEWS":
-//       return[...state, action.reviews];
-
-//       case "CREATE_REVIEW":
-//         return[...state, action.content];
-
-//       case "DELETE_REVIEW":
-//         return[...state].filter(elem => elem.id !== action.payload);
-  
-//     default:
-//       return state;
-//   }
-// }
-
-// function contentReducer(state = { contents: [], reviews: []}, action){
-//   switch (action.type) {
-//     case "GET_CONTENT":
-//       return{
-//         ...state,
-//         books: [...state.books],
-//         contents: [...state.contents, action.contents]
-//       };
-//       case "GET_REVIEWS":
-//         return{
-//           ...state,
-//           contents: [...state.contents],
-//           reviews: [...state.reviews, action.reviews]
-//         };
-//       case "CREATE_CONTENT":
-//         return{
-//           ...state,
-//           reviews: [...state.reviews],
-//           contents: [...state.contents, action.content]
-//         };
-//       case "DELETE_CONTENT":
-//         return{
-//           ...state,
-//           reviews: action.reviews
-//         };
-  
-//     default:
-//       return state;
-//   }
-// }
