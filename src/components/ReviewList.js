a7efcaf4e6f2533449973ca282f60d1123b2f5aa
Antonio <atcarter005@gmail.com>
2022-01-06 11:26:35 -0600

    remove unused code/comments

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,12 +1,9 @@
-// import React, { Component } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Review from './Review';
 import { deleteReview } from '../actions/reviewActions';
 
 export default function ReviewList (props) {
-  // const reviewsTotal = useSelector((state) => state.reviews)
-  // const reviews = reviewsTotal.filter(review => review.content_id === props.content_id)
+
   const dispatch = useDispatch()
   const handleDelete = (reviewID) => {
     dispatch(deleteReview(reviewID))
@@ -31,27 +28,3 @@ export default function ReviewList (props) {
   )
 
 }
-
-// class ReviewList extends Component {
-
-//   render() {
-//     return (
-//       <div className={`${this.props.content_id}-reviews`}>
-//         <h4>Review List</h4>
-//         {this.props.reviews && this.props.reviews.map(review =>
-//           <Review 
-//             key={`review-${review.id}`} 
-//             id={review.id}
-//             content_id={review.content_id} 
-//             username={review.username}
-//             rating={review.rating}
-//             description={review.description}
-//           />
-//         )}
-//       </div>
-//     )
-//   }
-
-// }
-
-// export default ReviewList;
