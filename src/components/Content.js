653a7410af236df5fd174ba6288627a2b6876e83
Antonio <atcarter005@gmail.com>
2022-01-07 09:01:29 -0600

    live coding exercise

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,11 +4,25 @@ import ReviewList from './ReviewList';
 
 class Content extends Component {
 
+
+  // handleClick(event) {
+    
+  // }
+
   render() {
 
     return (
       <div id={`content-${this.props.id}`}>
         <h3>{`${this.props.title} (${this.props.year})`}</h3>
+        <button onClick={(event)=>{
+          // parseInt(event.target.innerText) + this.props.inc
+          event.target.innerText =  parseInt(event.target.innerText) + this.props.inc
+
+          }}>
+          0
+        </button>
+        {/* <button onClick={this.handleClick}>0</button> */}
+
         <i>{this.props.content_type}</i>
         <p>{this.props.details}</p>
         <button onClick={() => this.props.delete(this.props.id)}> Delete {this.props.title} </button>
--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchContents } from '../actions/contentActions';
 import { fetchReviews } from '../actions/reviewActions'
 import Content from './Content'
@@ -27,12 +27,20 @@ export default function ContentList() {
     }
   }, [])
 
+  //Add a high level input that takes a number, and add buttons to each title that will increment when pushed by the amount in the input 
+
+  const [incrementValue, setIncrement] = useState(0)
+  // const handleChange((event) => {
+
+  // })
+
 
 
  
   return (
     <div className='content-list'>
       <h2>Content List</h2>
+      <input type="number" onChange={(event)=> setIncrement(parseInt(event.target.value))} />
       {contents && contents.map(content => (
         <Content 
           key={`content-${content.id}`} 
@@ -43,6 +51,7 @@ export default function ContentList() {
           details={content.details} 
           reviews={reviews.filter(review => review.content_id === content.id)}
           delete={handleDelete}
+          inc={incrementValue}
         />
       ))}
     </div>
